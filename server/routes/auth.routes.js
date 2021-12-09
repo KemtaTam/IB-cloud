1c227a933cc007375a9778529945bb302bc34143
Максим <maksim_bulavin@mail.ru>
2021-12-09 14:29:06 +0700

    Registration and authentication done

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,8 @@
 const Router = require("express");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");	//для хранения паролей в хеше
+const config = require("config");
+const jwt = require("jsonwebtoken");
 const {check, validationResult} = require("express-validator");	//валидация данных, чтобы скипать пустые пароли или невалидные маийлы
 const router = new Router();
 
@@ -25,10 +27,10 @@ router.post (
 				return res.status(400).json({message: "Uncorrect request", errors});
 			}
 
-			const {email, password} = req.body	//параметры из тела запроса
-			const candidate = await User.findOne({email});	//существует ли такой пользователь в базе
-
+			const {email, password} = req.body	//получаем параметры из тела запроса
+			const candidate = await User.findOne({email});	//существует ли пользователь с таким email в базе
 			if(candidate) return res.status(400).json({message: `User with email ${email} already exist`});
+
 			const hashPassword = await bcrypt.hash(password, 15);	//функция hash - асинхронная
 			const user = new User({email, password: hashPassword});
 			await user.save();	//сохранение пользователя в базе данных (операция асинхронная)
@@ -36,6 +38,39 @@ router.post (
 
 		} catch (error) {
 			console.log(error);
+			res.send({message: "Server error"});	//ответ пользователю
+		}
+	}
+)
+
+router.post ('/login', 
+	async (req, res) => {
+		try {
+			const {email, password} = req.body;
+			const user = await User.findOne({email});
+			if(!user) return res.status(404).json({message: "User not found"});
+
+			//если пользователь найден, нужно сравнить пароль из запроса с паролем из бд
+			//compareSync сравнивает зашифрованный пароль с не зашифрованным
+			const isPassValid = bcrypt.compareSync(password, user.password);
+			if(!isPassValid) return res.status(404).json({message: "Invalid password"});
+			//первый параметр - объект с данными, которые хотим поместить в токен
+			//второй параметр - секретный ключ из папки config
+			//третий параметр - объект, указывающий сколько времени токен будет существовать
+			const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"});	//создание токена
+			//после создания токена его необходимо вернуть обратно на клиент
+			return res.json({
+				token,
+				user: {
+					id: user.id,
+					email: user.email,
+					diskSpace: user.diskSpace,
+					usedSpace: user.usedSpace,
+					avatar: user.avatar
+				}
+			})
+		} catch (error) {
+			console.log(error); 
 			res.send({message: "Server error"});	//ответ пользователю
 		}
 	}
